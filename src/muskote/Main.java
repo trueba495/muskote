e845aec31d3bae3da0cfd30d94e89e78197f3cbf
Esteban Trueba <estebantrueba495@gmail.com>
2013-09-09 13:16:27 +0200

    Initial Commit

---
+++ src/muskote/Main.java
@@ -0,0 +1,13 @@
+package muskote;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/*Just a useless comment to test how git is working*/
+		System.out.println("This is Muskote");
+	}
+
+}
